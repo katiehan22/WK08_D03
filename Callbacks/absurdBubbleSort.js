583a4d979560d58ae889b516f103a24976ad4937
katiehan22 <katiehan22@gmail.com>
2022-09-28 16:22:58 -0700

    Finish bubblesort

--- Callbacks/absurdBubbleSort.js
+++ Callbacks/absurdBubbleSort.js
@@ -11,7 +11,7 @@ function askIfGreaterThan(el1, el2, callback) {
     } else {
       callback(false);
     }
-    rl.close();
+    // rl.close();
   })
 }
 
@@ -25,9 +25,25 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
         [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
         madeAnySwaps = true;
       }
+      return innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
     })
-    return innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
   }
 }
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if(madeAnySwaps === true) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  return outerBubbleSortLoop(true);
+}
+
+absurdBubbleSort([3, 2, 1], function (arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  rl.close();
+});
 
 // askIfGreaterThan(1, 3, (boolean) => console.log(boolean));
