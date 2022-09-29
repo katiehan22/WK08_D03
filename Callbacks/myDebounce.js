049fbc6a408cb380ec8d9f4c75f676c09beb1c85
katiehan22 <katiehan22@gmail.com>
2022-09-28 17:21:02 -0700

    Add debounce

---
+++ Callbacks/myDebounce.js
@@ -0,0 +1,44 @@
+Function.prototype.myDebounce = function(interval) {
+  let clear;
+  return () => {
+    clearTimeout(clear);
+    clear = setTimeout(this, interval);
+  }
+}
+
+class SearchBar {
+  constructor() {
+    this.query = "";
+
+    this.type = this.type.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  type(letter) {
+    this.query += letter;
+    this.search();
+  }
+
+  search() {
+    console.log(`searching for ${this.query}`);
+  }
+}
+
+const searchBar = new SearchBar();
+searchBar.search = searchBar.search.myDebounce(2000);
+
+const queryForHelloWorld = () => {
+  searchBar.type("h");
+  searchBar.type("e");
+  searchBar.type("l");
+  searchBar.type("l");
+  searchBar.type("o");
+  searchBar.type(" ");
+  searchBar.type("w");
+  searchBar.type("o");
+  searchBar.type("r");
+  searchBar.type("l");
+  searchBar.type("d");
+};
+
+queryForHelloWorld();
